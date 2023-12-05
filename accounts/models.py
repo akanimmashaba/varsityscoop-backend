1e2cb69bfe3086d913bee2833b8054399b7d4b20
Akani Mashaba <akanimmashaba@gmail.com>
2023-12-05 22:21:56 +0200

    modified:   accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -52,7 +52,7 @@ class Account(AbstractBaseUser,PermissionsMixin):
 
 
     id = models.UUIDField(primary_key = True,default = uuid.uuid4, editable = False) 
-    username = models.CharField(max_length=150,unique=True,)
+    username = models.SlugField(max_length=150,unique=True)
     email = models.EmailField(verbose_name='email address',max_length=255,unique=True,)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
