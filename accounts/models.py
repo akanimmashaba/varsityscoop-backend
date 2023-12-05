cdf62cb5f28ad92a0237fee9ea0973751b0cc2a1
Akani Mashaba <akanimmashaba@gmail.com>
2023-12-05 16:14:13 +0200

    added accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from django.contrib.auth.models import ( BaseUserManager, AbstractBaseUser)
+from django.db.models.fields import related
+import uuid 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from django.urls import reverse
 
 # Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            username=username,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class UserAccount(AbstractBaseUser):
+    id = models.UUIDField(primary_key = True,default = uuid.uuid4, editable = False) 
+    username = models.CharField(max_length=150)
+    email = models.EmailField(verbose_name='email address',max_length=255,unique=True,)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return str(self.user)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
