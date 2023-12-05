76fb38f0267e90610f704be502e68cbad0f14c16
Akani Mashaba <akanimmashaba@gmail.com>
2023-12-05 18:40:33 +0200

    accounts models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Account, Profile
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'user_type','is_superuser','is_regular','is_staff','is_admin', 'is_active',)
+    list_filter = ('is_superuser', 'is_admin', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('username',)}),
+        ('groups', {'fields': ('groups',)}),
+        ('Permissions', {'fields': ('is_admin','is_superuser', 'is_active')}),
+    )
+    
+    inlines = (UserProfileInline,)
+    search_fields = ('email', 'username',)
+    ordering = ('email',)
+
+
+class AbstractGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+
+
+admin.site.register(Account, CustomUserAdmin)
+admin.site.register(Profile)
--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import ( BaseUserManager, AbstractBaseUser)
-from django.db.models.fields import related
+from django.contrib.auth.models import ( BaseUserManager, AbstractBaseUser,PermissionsMixin)
 import uuid 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 from django.urls import reverse
+from django.contrib.auth.models import Group
 
-# Create your models here.
+class AccountManager(BaseUserManager):
+    use_in_migrations = True
 
-class MyUserManager(BaseUserManager):
     def create_user(self, email, username, password=None):
         """
         Creates and saves a User with the given email, date of
@@ -38,22 +38,38 @@ class MyUserManager(BaseUserManager):
             username=username,
         )
         user.is_admin = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
 
-class UserAccount(AbstractBaseUser):
+class Account(AbstractBaseUser,PermissionsMixin):
+    class UserTypeOptions(models.TextChoices):
+        REGULAR = "R", ("Regular")
+        WRITTER = "W", ("Writer")
+        EDITOR = "E", ("Editor")
+        MANAGER = "M", ("Manager")
+
+
     id = models.UUIDField(primary_key = True,default = uuid.uuid4, editable = False) 
-    username = models.CharField(max_length=150)
+    username = models.CharField(max_length=150,unique=True,)
     email = models.EmailField(verbose_name='email address',max_length=255,unique=True,)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+    user_type = models.CharField(max_length=1,choices=UserTypeOptions.choices, default=UserTypeOptions.REGULAR)
 
-    objects = MyUserManager()
+
+
+    objects = AccountManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+
+    class Meta:
+        verbose_name = "Account"
+        verbose_name_plural = "Accounts"
 
     def __str__(self):
         return self.username
@@ -69,16 +85,36 @@ class UserAccount(AbstractBaseUser):
         return True
 
     @property
+    def is_creator(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        if self.user_type != self.UserTypeOptions.REGULAR:
+            return True
+        return False
+    
+    @property
+    def is_regular(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        if self.user_type == self.UserTypeOptions.REGULAR:
+            return True
+        return False
+    
+    @property
     def is_staff(self):
         "Is the user a member of staff?"
         # Simplest possible answer: All admins are staff
         return self.is_admin
-
-
+    
+    
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
 
     def __str__(self):
         return str(self.user)
