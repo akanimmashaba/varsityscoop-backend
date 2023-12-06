c2ebdb80826ee70f72b867fee695502bdbdf9d06
Akani Mashaba <akanimmashaba@gmail.com>
2023-12-06 11:58:29 +0200

    modified:   accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -114,6 +114,8 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
+    avator = models.ImageField(upload_to='avator/', blank=True)
+
 
     class Meta:
         verbose_name = "Profile"
